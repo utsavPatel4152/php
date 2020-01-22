33ea29fc60912c287e6f90a68e2a73fbc517ef4f
utsav <utsav.ump@gmail.com>
2020-01-22 18:27:37 +0530

    Updated LCM

--- 22_Jan_Exercise/LCM.php
+++ 22_Jan_Exercise/LCM.php
@@ -2,37 +2,30 @@
 
     // Program to find the LCM of two numbers.
 
-    $number1 = 15;
-    $number2 = 25;
+    $number1 = 7;
+    $number2 = 5;
+    $counter = 1;
+    $flag = 0;
 
-    // if ($number1 > $number2) {
-    //     $largeNumber = $number1;
-    //     $smallNumber = $number2;
-    // }
-    // else {
-    //     $largeNumber = $number2;
-    //     $smallNumber = $number1;
-    // }
-
-    function hcf($) {
-        $hcf = 1;
-        for ($i = 2; $i < $number1 && $i < $number2 ; $i++) { 
-            if ($number1 % $i == 0 && $number2 % $i == 0) {
-                $hcf = $i;
-            }
-        }
-        return $hcf;
+    if ($number1 > $number2) {
+        $largeNumber = $number1;
+        $smallNumber = $number2;
+    }
+    else {
+        $largeNumber = $number2;
+        $smallNumber = $number1;
     }
 
-    for ($i = $largeNumber;  ; $i++) { 
+    while ($flag == 0) {
+        $answer = $counter * $smallNumber;
 
-        if ($number1 % $i == 0 && $number2 % $i == 0) {
-            $hcf = $i;
-            break;
+        if ($answer % $largeNumber == 0) {
+            $flag = 1;
         }
 
+        $counter++;
     }
 
-    echo 'LCM of '.$number1.' and '.$number2.': ';
+    echo 'LCM of '.$number1.' and '.$number2.': '.$answer;
 
 ?>
